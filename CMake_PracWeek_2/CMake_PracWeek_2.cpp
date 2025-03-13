5f76e0dadd5a6467ea2e0bdf246b62e13eced0ab
phecy004 <phecy004@ML-GP106-11>
2025-03-13 12:28:12 +1030

    Add project files.

---
+++ CMake_PracWeek_2/CMake_PracWeek_2.cpp
@@ -0,0 +1,12 @@
+﻿// CMake_PracWeek_2.cpp : Defines the entry point for the application.
+//
+
+#include "CMake_PracWeek_2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
